bb921e627ceeab5d2dafee63af01aeefc2635fa5
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-06-09 16:17:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b, int gcdValue) {
+    return a * (b / gcdValue);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int gcdValue = gcd(a, b);
+    int lcmValue = lcm(a, b, gcdValue);
+
+    cout << gcdValue << endl;
+    cout << lcmValue << endl;
+
+    return 0;
+}
