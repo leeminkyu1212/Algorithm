1b9d738b2c5cb3c2fb8d64131ef12ac5c628e78d
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-06-16 10:54:09 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void generateSequence(vector<int>& sequence, int start, int N, int M) {
+    if (sequence.size() == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << sequence[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i <= N; i++) {
+        sequence.push_back(i);
+        generateSequence(sequence, 1, N, M);
+        sequence.pop_back();
+    }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> sequence;
+    generateSequence(sequence, 1, N, M);
+    return 0;
+}
