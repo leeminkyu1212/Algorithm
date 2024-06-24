0e8f5d7512d5c6c40257128afc1f4e4dcf2d3a12
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-06-24 10:08:25 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,39 +1,43 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-long long ans;
-long long N, K;
-long long list[10000];
+long long getMaxCableLength(vector<long long>& cables, int K, int N) {
+    long long low = 1;
+    long long high = *max_element(cables.begin(), cables.end());
+    long long result = 0;
 
-int main()
-{
-	cin >> K >> N;
-	long long Max = 0;
-	for (int i = 0; i < K; i++)
-	{
-		cin >> list[i];
-		Max = max(Max, list[i]);
-	}
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long count = 0;
 
-	long long left = 1, right = Max, mid;
+        for (int i = 0; i < K; i++) {
+            count += cables[i] / mid;
+        }
 
-	while (left <= right)
-	{
-		mid = (left + right) / 2;
-		long long now = 0;
-		for (int i = 0; i < K; i++){
-			now += list[i] / mid;
-		}
-		if (now >= N){
-			left = mid + 1;
-			ans = max(ans, mid);
-		}
-		else{
-			right = mid - 1;
-		}
-	}
+        if (count >= N) {
+            result = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
 
-	cout << ans << '\n';
+    return result;
+}
+
+int main() {
+    int K, N;
+    cin >> K >> N;
+
+    vector<long long> cables(K);
+    for (int i = 0; i < K; i++) {
+        cin >> cables[i];
+    }
+
+    cout << getMaxCableLength(cables, K, N) << endl;
+
+    return 0;
 }
