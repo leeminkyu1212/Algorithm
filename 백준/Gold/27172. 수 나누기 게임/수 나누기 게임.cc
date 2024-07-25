0c06ae1e784d7821353cc22f00e97effa1b1096e
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-07-25 23:15:40 +0900

    [Gold V] Title: 수 나누기 게임, Time: 44 ms, Memory: 10728 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> numbers;
+int scores[1000001];
+int cards[1000001];
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		numbers.push_back(temp);
+		cards[temp] = 1;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = numbers[i] * 2; j < 1000001; j += numbers[i])
+		{
+			if (cards[j] == 1)
+			{
+				scores[numbers[i]]++;
+				scores[j]--;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+		cout << scores[numbers[i]] << " ";
+
+	return 0;
+}
