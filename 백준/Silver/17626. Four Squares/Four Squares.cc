07134c706c0c4853373037f73a9f24e42483a104
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-08-07 23:38:31 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j * j <= i; ++j) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    
+    cout << dp[n] << endl;
+    return 0;
+}
