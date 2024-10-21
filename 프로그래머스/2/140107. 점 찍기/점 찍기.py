50e07cdb77d9c0690c14e53cdb7b9a32392384c6
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-10-21 12:32:26 +0900

    [level 2] Title: 점 찍기, Time: 666.54 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(k, d):
+    answer = 0
+    
+    for i in range(0, d + 1, k):
+        max_y = math.isqrt(d**2 - i**2)
+        answer += (max_y // k) + 1
+    
+    return answer
