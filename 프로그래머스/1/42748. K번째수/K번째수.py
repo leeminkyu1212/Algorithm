13c0e1690ddbcb15ee1d736ed96fc67ea5d088d8
이민규 <58069418+leeminkyu1212@users.noreply.github.com>
2024-10-30 01:13:51 +0000

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,13 @@
+def solution(array, commands):
+
+    answer = []
+    
+    for command in commands:
+        new_list=array[command[0]-1:command[1]]
+        new_list.sort()
+        answer.append(new_list[command[2]-1])
+    
+    
+    
+    
+    return answer
